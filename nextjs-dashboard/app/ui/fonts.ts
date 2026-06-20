daa03667c4abc60b447a0283df653640a8d41f31
Prince Raj <princeraj@Princes-MacBook-Pro.local>
2026-06-20 19:29:12 +0545

    nxt starter

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,4 +1,4 @@
-import {Inter, Lusitana} from 'next/font/google';
-// import {Lusitana} from 'next/font/google'
+import {Inter} from 'next/font/google';
+import {Lusitana} from 'next/font/google'
 export const inter = Inter({subsets:['latin']});
-export const lusitana = Lusitana({weight:['400', '700'], subsets:['latin']})   
+export const lustiana = Lusitana({weight:['400', '700'], subsets:['latin']})   
